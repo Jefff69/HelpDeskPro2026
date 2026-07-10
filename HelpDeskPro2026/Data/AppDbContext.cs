12643a2e2a24c12e7777d1afbf132f8109f0a1e4
Jefff69 <jeffrey.am.18@gmail.com>
2026-07-10 17:07:16 -0600

    Add project files.

---
+++ HelpDeskPro2026/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HelpDeskPro2026.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDeskPro2026.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rol> Roles { get; set; }
+    }
+}
---
+++ HelpDeskPro2026/Models/Rol.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDeskPro2026.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
