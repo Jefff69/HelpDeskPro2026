136448c26193cf6c9f7076e5ac6cbe9ca18279a4
Jefff69 <jeffrey.am.18@gmail.com>
2026-08-18 11:04:31 -0600

    NewFilters

--- HelpDeskPro2026/ViewModels/TicketFilterViewModel.cs
+++ HelpDeskPro2026/ViewModels/TicketFilterViewModel.cs
@@ -4,20 +4,30 @@ namespace HelpDeskPro2026.Models.ViewModels
 {
     public class TicketFilterViewModel
     {
+        // Filtros
         public int? SistemaId { get; set; }
 
         public int? EstadoId { get; set; }
 
         public int? SolicitanteId { get; set; }
 
+        public DateTime? FechaDesde { get; set; }
 
+        public DateTime? FechaHasta { get; set; }
+
+
+        // Listas para los filtros
         public IEnumerable<SelectListItem> Sistemas { get; set; }
             = new List<SelectListItem>();
 
         public IEnumerable<SelectListItem> Estados { get; set; }
             = new List<SelectListItem>();
 
+        public IEnumerable<SelectListItem> Solicitantes { get; set; }
+            = new List<SelectListItem>();
 
+
+        // Resultados
         public List<TicketListItemViewModel> Tickets { get; set; }
             = new();
     }
